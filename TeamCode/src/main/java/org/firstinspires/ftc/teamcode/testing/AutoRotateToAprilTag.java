c55624949375844e8affbd6660eb381cad93356a
Screw-divers <Jona10112010@gmail.com>
2026-03-04 13:32:49 +0200

    added auto allign test (gotta test alot and change PD values.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/AutoRotateToAprilTag.java
@@ -0,0 +1,106 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import android.util.Range;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.mechanisms.MecanumDrive;
+import org.firstinspires.ftc.teamcode.mechanisms.Vision;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+@Autonomous (name = "Vision test", group = "test")
+public class AutoRotateToAprilTag extends OpMode {
+
+    Vision aprilTagWebcam = new Vision();
+    MecanumDrive Drive = new MecanumDrive();
+
+    double foward, strafe ,rotate;
+
+
+    double kP = 0.002;
+    double error = 0;
+    double lastError = 0;
+    double goal = 0;
+    double Tolerance = 0.5;
+    double kD = 0.0002;
+    double time = 0;
+    double lastTime = 0;
+    boolean saw = false;
+
+    public void start()
+    {
+        resetRuntime();
+        time = getRuntime();
+    }
+
+    public void init()
+    {
+        aprilTagWebcam.init(hardwareMap, telemetry);
+        Drive.init(hardwareMap);
+    }
+
+    public void loop()
+    {
+        foward = -gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        if (gamepad1.right_trigger_pressed)
+        {
+            aprilTagWebcam.update();
+            AprilTagDetection id20 = aprilTagWebcam.getTagById(20);
+
+            if (id20 != null)
+            {
+                saw = true;
+                error = goal - id20.ftcPose.bearing;
+
+                if (Math.abs(error) < Tolerance)
+                {
+                    rotate = 0;
+                }
+                else
+                {
+                    double pTerm = error * kP;
+
+                    time = getRuntime();
+                    double dT = time - lastTime;
+                    double dTerm = ((error - lastError) / dT) * kD;
+                    rotate = com.qualcomm.robotcore.util.Range.clip(pTerm + dTerm,-0.4,0.4);
+
+                    lastError = error;
+                    lastTime = time;
+                }
+            }
+            else
+            {
+                saw = false;
+                lastTime = getRuntime();
+                lastError = 0;
+            }
+        }
+        else
+        {
+            lastTime = getRuntime();
+            lastError = 0;
+        }
+
+        Drive.drive(foward,strafe,rotate);
+
+
+        if (saw)
+        {
+            telemetry.addLine("AUTO ALLIGNING");
+            telemetry.addData("Error", error);
+        }
+
+        telemetry.addData("P ", "%.4f", kP);
+        telemetry.addData("D ", "%.4f", kD);
+
+
+
+
+    }
+}
